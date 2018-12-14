4a31692c0884ec7b6a3d8a7b0ddc001df04cd154
pader <ypnow@163.com>
2018-12-15 02:58:21 +0800

    init vuetify with vue-cli3 ui.

---
+++ test-ui/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN VNote</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+
+    <v-navigation-drawer
+        class="blue lighten-3"
+        dark
+        permanent
+    >
+      <v-list>
+        <v-list-tile
+            v-for="item in items"
+            :key="item.title"
+            @click=""
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+		items: [
+			{ title: 'Dashboard', icon: 'dashboard' },
+			{ title: 'Account', icon: 'account_box' },
+			{ title: 'Admin', icon: 'gavel' }
+		]
+    }
+  }
+}
+</script>
